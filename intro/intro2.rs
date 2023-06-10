3aadc4e46f049a51aca30dc2640cb8f3b0c7dc74
Lenny Belardo <raoulgheletus@Raouls-MacBook-Pro.local>
2023-06-10 20:28:02 +0300

    intro2: added a string to the println!  macro - a value was expected in it; practiced adding comments from the rust by example book

--- intro/intro2.rs
+++ intro/intro2.rs
@@ -2,8 +2,13 @@
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    println!("Hello {}!");
+    println!("Hello {}!", "World");
+
+    // comments practice
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+    let x = 5 + 90 + 5;
+    println!("Is `x` 10 or 100? x = {}", x);
 }
