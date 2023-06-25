283e1aedb29397ecb6362275850c2b59a58d5663
Lenny Belardo <gheletus_raoul@yahoo.com>
2023-06-25 19:19:17 +0300

    conversions::from_str: implemented some string manipulation logic in the FromStr trait's from function

--- conversions/from_str.rs
+++ conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,30 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let person: Vec<&str> = s.split(",").collect();
+
+        if s.len() == 0 {
+            Err(ParsePersonError::Empty)
+        } else if person.len() != 2 {
+            Err(ParsePersonError::BadLen)
+        } else {
+            let (name, age) = match &person[..] {
+                [name, age] => (
+                    name.to_string(),
+                    age.parse().map_err(ParsePersonError::ParseInt)?,
+                ),
+                _ => return Err(ParsePersonError::BadLen),
+            };
+
+            if name.is_empty() {
+                return Err(ParsePersonError::NoName);
+            }
+
+            Ok(Person {
+                name,
+                age,
+            })
+        }
     }
 }
 
